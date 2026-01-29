3b599b2e580341bd49501d1e7b06585d5cd278e1
fish <-->
2026-01-29 14:04:02 +0800

    fix： 处理接口调用config

--- bajiecloud-customer-server/src/main/java/com/bajiezu/cloud/customer/config/RpcConfiguration.java
+++ bajiecloud-customer-server/src/main/java/com/bajiezu/cloud/customer/config/RpcConfiguration.java
@@ -1,11 +1,12 @@
 package com.bajiezu.cloud.customer.config;
 
+import com.bajiezu.cloud.marketing.api.vip.MarketingVipGradeApi;
 import com.bajiezu.cloud.marketing.api.vip.MarketingVipIntegralTaskApi;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(value = "customerRpcConfiguration", proxyBeanMethods = false)
-@EnableFeignClients(clients = {MarketingVipIntegralTaskApi.class})
+@EnableFeignClients(clients = {MarketingVipIntegralTaskApi.class, MarketingVipGradeApi.class})
 public class RpcConfiguration {
 
 }
