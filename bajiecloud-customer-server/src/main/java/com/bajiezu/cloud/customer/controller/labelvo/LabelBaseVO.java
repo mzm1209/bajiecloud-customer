f509195eecb834246371aac3737a61b4ca20e64d
sunwenqiang-js <--list>
2026-01-30 11:19:49 +0800

    feat:标签名称和备注长度调整

--- bajiecloud-customer-server/src/main/java/com/bajiezu/cloud/customer/controller/labelvo/LabelBaseVO.java
+++ bajiecloud-customer-server/src/main/java/com/bajiezu/cloud/customer/controller/labelvo/LabelBaseVO.java
@@ -11,14 +11,12 @@ import org.apache.commons.lang3.StringUtils;
 public class LabelBaseVO {
 
     @Schema(description = "标签名称", requiredMode = Schema.RequiredMode.REQUIRED, example = "xxx")
-    @Size(max = 30, message = "标签名称长度不能超过 50 个字符")
     private String name;
 
     @Schema(description = "标签类型 1: 手动添加 默认传1", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private Integer labelType;
 
     @Schema(description = "备注",  example = "xxx")
-    @Size(max = 100, message = "备注长度不能超过 200 个字符")
     private String remark;
 
     public void validaParam() {
